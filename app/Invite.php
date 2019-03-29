bb73f89b6b9253e66c5fa513793ba57ed0ed25cb
Daniel Ortega <danielortega@Daniels-MacBook-Pro.local>
2019-03-29 17:46:20 -0600

    Invites

--- app/Invite.php
+++ app/Invite.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Design extends Model
 {
+    public $timestamps = false;
+    
     protected $fillable = [
         'name','invitation','tickets','confirm','user'
     ];
